61e627cb06b801c09f6dc483d441a42c6c7c689a
wellion-nyi <nyinyinaing15@outlook.com>
2023-06-29 20:29:10 +0630

    add commit line

--- lib/Api/api.dart
+++ lib/Api/api.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Callme {
+  // This is UrL
   final String apiUrl = "https://randomuser.me/api/?results=10";
 
   Future<List<dynamic>> fetchUsers() async {
